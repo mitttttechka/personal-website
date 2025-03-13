48039b85069eeda7b71d9bb2e3241f981ee4e085
Mitttttechka <58935601+mitttttechka@users.noreply.github.com>
2025-03-13 19:20:27 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple button click event
+document.addEventListener('DOMContentLoaded', () => {
+    const contactBtn = document.getElementById('contact-btn');
+    contactBtn.addEventListener('click', () => {
+      alert("Thanks for your interest! You can reach me at: [your_email@example.com]");
+    });
+  });
+  
